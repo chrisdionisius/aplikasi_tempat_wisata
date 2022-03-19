1fddc8b02386907fe2d904d52d2a58a504e78931
Dionisius Damarta Yapenrui <chrisdionisius@gmail.com>
2022-03-19 15:30:56 +0700

    add detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:aplikasi_tempat_wisata/pages/detail_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const DetailScreen(),
     );
   }
 }
