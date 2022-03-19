ba1d60238520ab77e0af34482845f1c01545b504
Dionisius Damarta Yapenrui <chrisdionisius@gmail.com>
2022-03-19 18:49:54 +0700

    add assets and detail_screen second part

--- lib/pages/detail_screen.dart
+++ lib/pages/detail_screen.dart
@@ -5,66 +5,111 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const informationTextStyle = TextStyle(fontFamily: 'Oxygen');
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(top: 16.0),
-              child: const Text(
-                'Farm House Lembang',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.asset('assets/images/farm-house.jpg'),
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                child: const Text(
+                  'Farm House Lembang',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Staatliches',
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16.00),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: const <Widget>[
-                      Icon(Icons.calendar_today),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Text('Open Everyday')
-                    ],
-                  ),
-                  Column(
-                    children: const <Widget>[
-                      Icon(Icons.access_time),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Text('09:00 - 20:00')
-                    ],
-                  ),
-                  Column(
-                    children: const <Widget>[
-                      Icon(Icons.monetization_on),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Text('RP 25.000')
-                    ],
-                  ),
-                ],
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 16.00),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: const <Widget>[
+                        Icon(Icons.calendar_today),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(
+                          'Open Everyday',
+                          style: informationTextStyle,
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: const <Widget>[
+                        Icon(Icons.access_time),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(
+                          '09:00 - 20:00',
+                          style: informationTextStyle,
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: const <Widget>[
+                        Icon(Icons.monetization_on),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(
+                          'RP 25.000',
+                          style: informationTextStyle,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: const Text(
-                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.00),
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: const Text(
+                  'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16.00),
+                ),
               ),
-            )
-          ],
+              Container(
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
